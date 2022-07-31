73d4f8b0fe75c7f4c5f325e8c792381c37ac7ab9
Mahdi-Suleiman <mahdi.suleimann@gmail.com>
2022-07-31 12:15:22 +0300

    backup

---
+++ SurveyQuestionsConfigurator.Web/ViewModels/CreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyQuestionsConfigurator.Web.ViewModels
+{
+    public class CreateViewModel
+    {
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+
+        public dynamic Model { get; set; }
+        public Type ModelType { get; set; }
+    }
+}
